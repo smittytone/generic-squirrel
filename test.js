1ab199f477a914b6611162791a1fea85b9c99e98
Tony Smith <tony@electricimp.com>
2017-09-19 16:53:45 +0100

    Update test.js

--- test.js
+++ test.js
@@ -24,17 +24,85 @@ function setOptions(verb, path, token) {
 
 function initStream() {
   // Set up request
-  let req = http.request(setOptions('POST', 'logstream', acccesToken), (resp) => {
+  let req = https.request(setOptions('POST', 'logstream', acccesToken), (resp) => {
     let body = '';
     resp.setEncoding('utf8');
     resp.on('data', (chunk) => { body += chunk; });
     resp.on('end', () => {
       if (body.length > 0) {
-      
+        try {
+          let data = JSON.parse(body);
+          data = data.data;
+          if (resp.statusCode === 200) {
+            let url = data.attributes.url;
+            beginStream(url);
+          } else {
+            // API Error
+            console.log(`API ERROR: ${data.code} (${data.message}`);
+          }
+        } catch (err) {
+          console.log(err);
+        }
       } else {
         // HTTP Error
         showError(resp);
       }
     });
   });
- }
+  
+  // Send request
+  req.end();
+}
+
+function beginStream(url) {
+  // Set up stream
+  let urlarray = url.split('//');
+  urlarray = urlarray[1].split('/');
+  let path = '';
+  for (part of urlarray) {
+    path = path + part + '/';
+  }
+
+  let req = https.request(setOptions('GET', url, accessToken), (resp) => {
+    let body = '';
+    resp.setEncoding('utf8');
+    resp.on('data' (chunk) => { data += chunk; });
+    resp.on('end', () => {
+      if (resp.statusCode === 200) {
+        // Add device to stream
+        addDevice(path, device);
+      }
+    });
+  });
+
+  req.end();
+}
+
+function addDevice(path, id) {
+  let req = https.request(setOptions('PUT', path + id, accessToken), (resp) => {
+    let body ='';
+    resp.setEncoding('utf8');
+    resp.on('data', (chunk) => { body += chunk });
+    resp.on('end', () => {
+      try {
+        let data = JSON.parse(body);
+        data = data.data;
+        if (data.event === 'message') {
+          console.log(data.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+
+  req.write(JSON.stringify({
+    'id': id,
+    'type': 'device'
+  }));
+
+  req.end();
+}
+
+// RUNTIME
+initStream();
